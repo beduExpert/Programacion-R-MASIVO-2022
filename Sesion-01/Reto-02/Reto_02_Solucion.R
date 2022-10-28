234f26ccdc043be97fdcb3a0a51a35d25456c37b
Dillan <83663474+DillanAS@users.noreply.github.com>
2022-10-27 19:11:14 -0500

    Update Reto_02_Solucion.R

--- Sesion-01/Reto-02/Reto_02_Solucion.R
+++ Sesion-01/Reto-02/Reto_02_Solucion.R
@@ -1,17 +1,33 @@
-# Reto 2
+## RETO 02: LECTURA DE DATAFRAMES
 
-amazon.best <- read.csv("https://raw.githubusercontent.com/ecoronadoj/Sesion_1/main/Data/bestsellers%20with%20categories.csv")
+"En la gran mayoría de las sesiones estaremos utilizando distintas tablas de datos,
+por lo que es importante a prender a leerlas.
 
-# Convirtiendo el DF a transpuesto y en un data frame 
-tAmazon <- as.data.frame(t(amazon.best))
+Como recordarás, el formato .csv es uno de los más utilizados, por lo que nos 
+enfocaremos en ello, sin embargo, la mayoría de funciones para leer archivos 
+siguen la misma lógica.
 
-# Usando el nombre de los libros como el nombre de las columnas
-colnames(tAmazon) <- tAmazon[1,]
+La función read.csv() toma un argumento principal, que establece la ruta de sistema 
+o url al archivo .csv.
 
-row.names(tAmazon)
+Si vas a leer un archivo que se encuentra localmente en tu computadora, es importante
+que este se encuentre en la dirección actual de trabajo, la cual puedes consultar 
+con la función getwd(). Para cambiar la dirección de trabajo puedes usar la función 
+setwd(path)"
 
-# libro con mayor precio
-which.max(tAmazon["Price",])
+"A) Usando la función de lectura adecuada, lee el archivo que se encuentra en la 
+siguiente url"
+netflix <-read.csv("https://raw.githubusercontent.com/beduExpert/Programacion-R-Santander-2022/main/Sesion-01/Data/netflix_titles.csv")
 
-# libro con menor precio
-which.min(tAmazon["Price",])
+"B) Verifica la clase de la tabla, el número de variables y de observaciones, así 
+como el nombre de las variables"
+dim(netflix)
+class(netflix)
+names(netflix)
+
+"C) Usando indexación, selecciona sólo los datos de películas que fueron estrenadas 
+desde 2015 y hasta antes del 2019"
+net.2015 <- netflix[(netflix$release_year >= 2015 & netflix$release_year < 2019), ]
+
+"D) Por último, averigua que hace la siguiente función"
+View(net.2015)
