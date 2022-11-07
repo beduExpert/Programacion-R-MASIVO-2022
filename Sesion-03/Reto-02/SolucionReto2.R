bd2ab98ce918d09d670472f9df89662e36fcc634
Dillan <83663474+DillanAS@users.noreply.github.com>
2022-11-07 11:46:59 -0600

    Update SolucionReto2.R

--- Sesion-03/Reto-02/SolucionReto2.R
+++ Sesion-03/Reto-02/SolucionReto2.R
@@ -1,66 +1,28 @@
-# NAB. Reto 2
+## RETO 02: MEDIDAS DESCRIPTIVAS
+library(ggplot2)
 
+"Para este reto vamos a hacer uso del dataframe diamonds, disponible en la librería 
+de ggplot2. Realiza los siguientes incisos y responde a las preguntas:"
 
-# Full players stats from the 2014-2015 season + personal details such as height. weight, etc.
+"1) Calcula e interpreta las medidas de tendencia central de la variable price"
+library(DescTools)
+Mode(diamonds$price); median(diamonds$price); mean(diamonds$price)
 
-#The data was scraped and copied from:
- # http://www.basketball-reference.com/teams/
-#  and
-#http://stats.nba.com/leaders#!?Season=2014-15&SeasonType=Regular%20Season&StatCategory=MIN&CF=MING2&PerMode=Totals
-# En esta última página podrás encontrar el glosario 
+"2) Con base en tu resultado anteior, ¿qué se puede concluir respecto al sesgo del precio?"
+"R. Dado que moda < mediana < media, la distribución está sesgada a la derecha."
 
+"3) Calcula e interpreta la desviación estándar y los cuartiles de la distribución:"
+sd(diamonds$price) # El precio tiene una dispersión promedio, respecto a la media, de 3989.44
 
-# Leyendo los datos de la NBa, 
-nba <- read.csv("players_stats.csv")
-names(nba)
+quantile(diamonds$price, probs = c(0.25, 0.50, 0.75))
+# 25% de los diamantes tienen un precio de 950 o menos
+# 50% de los diamantes tienen un precio de 2401 o menos
+# 75% de los diamantes tienen un precio de 5324.25 o menos
 
-# 1. Histograma de los minuntos totales de losjugadores y la media
-mnba <-mean(nba$MIN)
-ggplot(nba, aes(MIN))+ 
-  geom_histogram(binwidth = 30, col="black", fill = "blue") + 
-  ggtitle("Histograma de Minutos por jugador", paste("Media=",mnba)) +
-  ylab("Frecuencia") +
-  xlab("Minutos") +
-  geom_vline(xintercept =  mnba, col = "red", lwd = 1.5, lty =2)+
-  theme_light()
+"4) Realiza un histograma de la variable precio. ¿Su distribución coincide con tu 
+conclusión de la pregunta 2?"
+hist(diamonds$price)
 
-# 2. Histograma de edad y media
-
-ma.nba <-mean(na.omit(nba$Age))
-
-ggplot(nba, aes(Age))+ 
-  geom_histogram(binwidth = 2, col="black", fill = "blue") + 
-  ggtitle("Histograma de Edad", paste("Media=",ma.nba)) +
-  ylab("Frecuencia") +
-  xlab("Edad") +
-  geom_vline(xintercept =  ma.nba, col = "red", lwd = 1.5, lty =2)+
-  theme_light()
-
-#3. Scatterplot de Peso y Altura
-
-p <- nba %>% ggplot(aes(Weight, Height)) +
-       geom_point()
-p
-
-(lmnba <- coef(lm(Height ~ Weight, data = nba)))
-#> (Intercept)          MIN 
-#>   -136.129102   1.193261 
-p + geom_abline(intercept = lmnba[1], slope = lmnba[2], col = "red", lwd = 1.5, lty = 2)
-
-#4. Jugador más alto
-(alto <- which.max(nba$Height))
-paste("El jugador más alto es:", nba$Name[alto],"con una altura de:" , round(nba$Height[alto]/100,2), "m")
-
-#5. Jugador más bajo
-(bajito <- which.min(nba$Height))
-paste("El jugador más bajito es:", nba$Name[bajito],"con una altura de:" ,round(nba$Height[bajito]/100,2), "m")
-
-#6. La altura promedio, (Hint: hay que utilizar na.omit en caso de presencia de NA´s)
-(altura.m <- mean(na.omit(nba$Height)))
-paste("La altura promedio es:", round(altura.m/100,2),"m")
-
-# 7. Scatterplot de Asistencias totales vs Puntos, con un face wrap por posición.
-nba %>% ggplot( aes(AST.TOV, PTS )) +
-  geom_point() + 
-  facet_wrap("Pos")
-             
+"5) Realiza un boxplot de la variable precio. ¿Su forma se relaciona con la distribución 
+mostrada por el histograma anteior? ¿Existen datos atípicos?"
+boxplot(diamonds$price)
