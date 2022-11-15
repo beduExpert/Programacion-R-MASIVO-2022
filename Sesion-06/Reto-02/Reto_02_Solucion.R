4c053637859dc7431bf0141ae023fb81d720bd2a
Dillan <83663474+DillanAS@users.noreply.github.com>
2022-11-15 16:31:11 -0600

    Update Reto_02_Solucion.R

--- Sesion-06/Reto-02/Reto_02_Solucion.R
+++ Sesion-06/Reto-02/Reto_02_Solucion.R
@@ -1,39 +1,38 @@
-# Reto 2. Proceso ARIMA(1, 1, 1)
+#Reto 02: Regresión logística
 
-# 1. Simula n = 1000 valores de un proceso ARIMA(1, 1, 1) con parámetros
-# ar = 0.6 y ma = 0.2
+"El Dataframe 'inseguridad_alimentaria' contiene información de la Encuesta Nacional 
+de Salud y Nutrición (ENSANUT) 2012, específicamente del módulo de condiciones socieconómicas 
+del hogar y del módulo de percepción de inseguridad alimentaria en el hogar.
 
-# 2. Ajusta un modelo Arima a la serie simulada para estimar los
-# parámetros y observa las estimaciones de los parámetros
+Dentro del dataframe se encuentran las siguientes variables, entre otras:
+- IA: 1 si el hogar presenta inseguridad alimentaria, 0 en otro caso
+- numpeho: Número de personas en el hogar
+- edadjef: Edad del jefe de familia
+- sexojef: 0 si el jefe familia es Hombre, 1 si es Mujer
+- añosedu: Años de educación del jefe de familia
+- alsa: Gasto semanal en gastos alimentos saludables
 
-# 3. Obtén el correlograma de los residuales del ajuste
+El objetivo es construir un modelo que nos permita estimar el impacto que tienen variables 
+socieonómicas en la probabilidad de que un hogar presente seguridad alimentaria.
 
-# 4. Realiza tres predicciones con ayuda del modelo ajustado y la función
-# `predict`
+Asegurate que tu modelo final sea aquel con el pseudo_r2 más alto e interpreta los 
+log odds del modelo estimado.
 
-# **Solución**
+NOTA: No olvides probar efectos de interacción"
 
-# A continuación, simulamos datos de un modelo ARIMA(1, 1, 1) 
+df <- read.csv("inseguridad_alimentaria.csv")
 
-set.seed(9)
-x <- arima.sim(model = list(order = c(1, 1, 1), 
-                            ar = 0.6, ma = 0.2), n = 1000)
+df$IA <- factor(df$IA, labels = c("No", "Si"))
+df$mened <- factor(df$mened, labels = c("No", "Si"))
+df$sexojef <- factor(df$sexojef, labels = c("H", "M"))
+summary(df)
 
-# ajustamos un modelo a la serie simulada para recuperar los parámetros 
-# estimados
+attach(df)
 
-fit <- arima(x, order = c(1, 1, 1))
+logistic.1 <- glm(IA ~ numpeho + edadjef + edadjef:sexojef + 
+                    añosedu + añosedu:sexojef + alsa + mened, family = binomial)
 
-# observamos las estimaciones de los parámetros
+summary(logistic.1)
 
-coefficients(fit)
-
-# obtenemos el correlograma de los residuales del ajuste
-
-acf(resid(fit), main = "")
-title(main = "Autocorrelaciones para los Residuales del Ajuste")
-
-# Llevamos a cabo tres predicciones con el modelo ajustado y la función `predict`
-
-pred <- predict(fit, n.ahead = 3)
-pred$pred
+pseudo_r2.1 <- (logistic.1$null.deviance - logistic.1$deviance)/logistic.1$null.deviance
+pseudo_r2.1
