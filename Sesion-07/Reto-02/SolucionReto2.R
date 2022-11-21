51f106148885258828b94bc35a8765f3853da016
Dillan <83663474+DillanAS@users.noreply.github.com>
2022-11-21 15:58:16 -0600

    Update SolucionReto2.R

--- Sesion-07/Reto-02/SolucionReto2.R
+++ Sesion-07/Reto-02/SolucionReto2.R
@@ -1,34 +1,45 @@
-library(rvest)
+#Reto 02: Proceso ARIMA(1,1,1) y predicción
 
-theurl <- "https://www.glassdoor.com.mx/Sueldos/data-scientist-sueldo-SRCH_KO0,14.htm"
+"La función arima.sim() también nos permite simular modelos de series de tiempo con 
+términos autorregresivos y de media móvil. En este ejemplo, vamos a estimar una serie de 200 obs,
+con un término autorregresivo y uno de media móvil, junto con sus coeficientes asociados"
+y <- arima.sim(model = list(order = c(1, 1, 1), ar = 0.8, ma = 0.3), n = 200)
+plot(y)
 
-file<-read_html(theurl)
+"Con la función arima.sim() y las demás vistas en la sesión, realiza lo siguiente:"
+# 1. Simula n = 1000 valores de un proceso ARIMA(1, 1, 1) con parámetros
+# ar = 0.6 y ma = 0.2
 
-tables<-html_nodes(file, "table")
-# Hay que analizar 'tables' para determinar cual es la posición en la lista que contiene la tabla, en este caso es la no. 4 
+# 2. Ajusta un modelo Arima a la serie simulada para estimar los
+# parámetros y observa las estimaciones de los parámetros
 
-table1 <- html_table(tables[1], fill = TRUE)
+# 3. Obtén el correlograma de los residuales del ajuste
 
+# 4. Realiza tres predicciones con ayuda del modelo ajustado y la función predict()
 
-table <- na.omit(as.data.frame(table1))
+# **Solución**
 
-str(table)
+# A continuación, simulamos datos de un modelo ARIMA(1, 1, 1) 
 
-#Removiendo caracteres inncesarios 
-a <- gsub("MXN","",table$Sueldo)
-a <- gsub("[^[:alnum:][:blank:]?]", "", a)
-a <- gsub("mes", "", a)
-a <- as.numeric(a)
-table$Sueldo <- a
+set.seed(9)
+x <- arima.sim(model = list(order = c(1, 1, 1), 
+                            ar = 0.6, ma = 0.2), n = 1000)
 
-#Removiendo caracteres inncesarios
-b <- gsub("Sueldos para Data Scientist en ", "", table$Cargo)
-table$Cargo <-b
+# ajustamos un modelo a la serie simulada para recuperar los parámetros 
+# estimados
 
-#Máximo sueldo
-max.sueldo <- which.max(table$Sueldo)
-table[max.sueldo,]
+fit <- arima(x, order = c(1, 1, 1))
 
-#Mínimo sueldo
-min.sueldo <- which.min(table$Sueldo)
-table[min.sueldo,]
+# observamos las estimaciones de los parámetros
+
+coefficients(fit)
+
+# obtenemos el correlograma de los residuales del ajuste
+
+acf(resid(fit), main = "")
+title(main = "Autocorrelaciones para los Residuales del Ajuste")
+
+# Llevamos a cabo tres predicciones con el modelo ajustado y la función `predict`
+
+pred <- predict(fit, n.ahead = 3)
+pred$pred
