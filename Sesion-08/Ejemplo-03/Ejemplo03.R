0324b258424609140a1d8da78e62c163610c68b9
Dillan <83663474+DillanAS@users.noreply.github.com>
2022-11-21 16:54:09 -0600

    Update and rename app.R to Ejemplo03.R

---
+++ Sesion-08/Ejemplo-03/Ejemplo03.R
@@ -0,0 +1,28 @@
+## EJEMPLO 03: LECTURA Y MANIPULACIÓN DE ARCHIVOS JSON
+"JSON es un acrónimo para JavaScript Object Notation, y fueron desarrollados e 
+implementados inicialmente dentro del lenguaje de programación JS.
+Los archivos JSON son un formago ligero de intercambio de datos que solucionó los 
+problemas presentados por el estándar XML, sustituyéndolo y potenciando su popularidad
+al punto en que el stándar JSON es aplicado y aceptado por múltiples lenguajes de programación,
+APIs y diversos sistemas.
+
+Un archivo JSON se caracteriza por almacenar valores (objetos de datos) a través de 
+claves (llaves) que funcionan como identificador del valor.
+
+Para leer y manipular archivos JSON en R necesitamos de la siguiente paquetería:"
+install.packages("rjson")
+library(rjson)
+
+"La función fromJSON() permite leer archivos JSON locales o en una url:"
+URL <- "https://tools.learningcontainer.com/sample-json-file.json"
+JsonData <- fromJSON(file= URL)
+
+class(JsonData)
+
+"Los JSON son interpretados por R como una lista (al igual que Python), por lo que 
+podemos manipularlos de la misma forma y aplicar todo lo que hemos visto en el curso:"
+str(JsonData)
+
+JsonData$Name
+is.vector(JsonData$Pets)
+JsonData$Pets[1]
